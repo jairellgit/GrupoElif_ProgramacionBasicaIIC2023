385bfcd2d04dbe4786252a0981e002939ceea38c
soficam <133938038+soficam@users.noreply.github.com>
2023-07-02 21:40:21 -0600

    Update index.py
    Opciones restantes añadidas (temporales)

--- index.py
+++ index.py
@@ -7,6 +7,17 @@ def start():
 
     if menuOption == 1:
         addRegistration()
+    elif menuOption == 2:
+        # Eliminar cuando ya esté listo:
+        print(">>> Opción no disponible aún. Regresando al menú principal...")
+        start() 
+    elif menuOption == 3:
+        # Eliminar cuando ya esté listo:
+        print(">>> Opción no disponible aún. Regresando al menú principal...")
+        start()
+    elif menuOption == 4:
+        print(">>> Saliendo al menú principal...")
+        start()
 
 
 start()
