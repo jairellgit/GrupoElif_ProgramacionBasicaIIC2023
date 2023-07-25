95cff717c63a29ecd32d0353a6f4f13552f3b413
soficam <133938038+soficam@users.noreply.github.com>
2023-07-24 19:07:05 -0600

    Update user_registration.py

--- codigo_fuente/user_registration.py
+++ codigo_fuente/user_registration.py
@@ -13,3 +13,19 @@ arrayTipoCambio = helpers.tipoDeCambio()
 minMoneyDolar = float(arrayTipoCambio[0]) #El primer dato del archivo de Conf Avanzada es el monto mínimo de deposito requerido en dolares
 minMoneyColon = float(arrayTipoCambio[1]) * minMoneyDolar #El dato del archivo Conf Avanza es el valor de 1 dolar a colones
 minMoneyBitcoin = float(arrayTipoCambio[2]) * minMoneyDolar #El dato del archivo Conf Avanza es el valor de 1 dolar a bitcoins
+
+def validateUserIdAttempts():
+    totalUserIdValidAttempts = 3
+    global userIdAttempts
+    userIdAttempts += 1
+
+    if (userIdAttempts == totalUserIdValidAttempts):
+        print(
+            f"\nHa excedido el máximo de {totalUserIdValidAttempts} intentos para ingresar un ID válido, volviendo al menú principal...")
+
+        # importante importar aquí y no al inicio para evitar una importación circular entre ambos módulos
+        helpers.returnToMainMenu()
+    else:
+        attemptsLeft = 3 - userIdAttempts
+        print("\n>>> ID de usuario inválido, mínimo cinco caracteres. Le quedan "+str(attemptsLeft)+" intentos.") 
+        addRegistration()
