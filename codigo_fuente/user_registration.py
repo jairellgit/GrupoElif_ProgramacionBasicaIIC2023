0db51463f3aba966bdd8b6a8f4fca368da5c1f83
soficam <133938038+soficam@users.noreply.github.com>
2023-07-24 19:08:03 -0600

    Update user_registration.py

--- codigo_fuente/user_registration.py
+++ codigo_fuente/user_registration.py
@@ -1,6 +1,7 @@
 import getpass
 import helpers 
 
+
 userIdAttempts = 0
 
 userId = ""
@@ -13,6 +14,7 @@ arrayTipoCambio = helpers.tipoDeCambio()
 minMoneyDolar = float(arrayTipoCambio[0]) #El primer dato del archivo de Conf Avanzada es el monto mínimo de deposito requerido en dolares
 minMoneyColon = float(arrayTipoCambio[1]) * minMoneyDolar #El dato del archivo Conf Avanza es el valor de 1 dolar a colones
 minMoneyBitcoin = float(arrayTipoCambio[2]) * minMoneyDolar #El dato del archivo Conf Avanza es el valor de 1 dolar a bitcoins
+
 
 def validateUserIdAttempts():
     totalUserIdValidAttempts = 3
@@ -29,3 +31,27 @@ def validateUserIdAttempts():
         attemptsLeft = 3 - userIdAttempts
         print("\n>>> ID de usuario inválido, mínimo cinco caracteres. Le quedan "+str(attemptsLeft)+" intentos.") 
         addRegistration()
+
+
+# def isUserExists(userId):
+    # Esta función booleana valida si un usuario existe o no
+
+
+def isValidUserId(userId):
+    minUserIdLenght = 5
+    return len(userId) >= minUserIdLenght
+
+
+def validateUserId(userId):
+    if isValidUserId(userId):
+        # if isUserExists(userId):
+        #  print("\nEste userId ya es ocupado por otro usuario, intente nuevamente...")
+        #  return validateUserIdAttempts()
+        return userId
+    validateUserIdAttempts()
+
+
+def getUserId():
+    userId = input("Ingrese su ID (debe contener al menos cinco caractéres):\n> ")
+    return validateUserId(userId)
+
