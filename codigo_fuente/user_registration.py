506ab7237a3994c01dd347fc025b6306738d19d2
soficam <133938038+soficam@users.noreply.github.com>
2023-08-20 21:43:15 -0600

    Update user_registration.py

--- codigo_fuente/user_registration.py
+++ codigo_fuente/user_registration.py
@@ -11,12 +11,7 @@ userName = ""
 userPin = 0 
 userDeposit = 0
 
-arrayTipoCambio = helpers.tipoDeCambio()
-
-minMoneyDolar = float(arrayTipoCambio[0]) #El primer dato del archivo de Conf Avanzada es el monto mínimo de deposito requerido en dolares
-minMoneyColon = float(arrayTipoCambio[1]) * minMoneyDolar #El dato del archivo Conf Avanza es el valor de 1 dolar a colones
-minMoneyBitcoin = float(arrayTipoCambio[2]) * minMoneyDolar #El dato del archivo Conf Avanza es el valor de 1 dolar a bitcoins
-
+arrayTipoCambio = helpers.confAvanzada()
 
 # >>> Escogencia de ID
 # Solicitar al usuario que cree su ID
