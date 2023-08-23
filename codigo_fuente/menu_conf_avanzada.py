ec34e7ab22bb2ae403d2bd695c012bc65aee551c
soficam <133938038+soficam@users.noreply.github.com>
2023-08-22 18:01:02 -0600

    Update menu_conf_avanzada.py
    Corrección para imprimir los valores actuales

--- codigo_fuente/menu_conf_avanzada.py
+++ codigo_fuente/menu_conf_avanzada.py
@@ -102,38 +102,43 @@ def advancedSettings():
                 deleteUser(userIdDelete)
             elif choice == 2:
                 print("\nMenú de Modificación de Valores del Sistema:")
-                print("1. Valor mínimo depósito inicial")
-                print("2. Tipo de cambio: valor del colon")
-                print("3. Tipo de cambio: valor del bitcoin")
-                print("4. Acumulado del tragamonedas")
-                print("5. Apuesta mínima tragamonedas")
-                print("6. Apuesta mínima blackjack")
+                print(f"1. Valor mínimo depósito inicial (Actualmente {arrayConfAvanzada[0]})")
+                print(f"2. Tipo de cambio: valor del colon (Actualmente {arrayConfAvanzada[1]})")
+                print(f"3. Tipo de cambio: valor del bitcoin (Actualmente {arrayConfAvanzada[2]})")
+                print(f"4. Acumulado del tragamonedas (Actualmente {arrayConfAvanzada[3]})")
+                print(f"5. Apuesta mínima tragamonedas (Actualmente {arrayConfAvanzada[4]})")
+                print(f"6. Apuesta mínima blackjack (Actualmente {arrayConfAvanzada[5]})")
                 
-                choiceMenu2 = int(input("\nSeleccione una opción a modificar: "))
-                if choiceMenu2 == 1:
-                    arrayConfAvanzada[0] = input("Ingrese el nuevo valor mínimo del depósito inicial: \n> $")
-                    print(">>> Valor modificado exitosamente.")
-                elif choiceMenu2 == 2:
-                    arrayConfAvanzada[1] = input("Ingrese el nuevo valor para el 'Tipo de cambio: valor del colon': \n> $")
-                    print(">>> Valor modificado exitosamente.")
-                elif choiceMenu2 == 3:
-                    arrayConfAvanzada[2] = input("Ingrese el nuevo valor para el 'Tipo de cambio: valor del bitcoin': \n> $")
-                    print(">>> Valor modificado exitosamente.")
-                elif choiceMenu2 == 4:
-                    arrayConfAvanzada[3] = input("Ingrese el nuevo valor para el 'Acumulado del tragamonedas': \n> $")
-                    print(">>> Valor modificado exitosamente.")
-                elif choiceMenu2 == 5:
-                    arrayConfAvanzada[4] = input("Ingrese el nuevo valor para la 'Apuesta mínima del tragamonedas': \n> $")
-                    print(">>> Valor modificado exitosamente.")
-                elif choiceMenu2 == 6:
-                    arrayConfAvanzada[5] = input("Ingrese el nuevo valor para la 'Apuesta mínima del blackjack': \n> $")
-                    print(">>> Valor modificado exitosamente.")
-                else:
-                    print(">>> Opción no válida.")
+                try:
+                    choiceMenu2 = int(input("\nSeleccione una opción a modificar: "))
+                    
+                    if choiceMenu2 == 1:
+                        arrayConfAvanzada[0] = input("Ingrese el nuevo valor mínimo del depósito inicial: \n> $")
+                        print(">>> Valor modificado exitosamente.")
+                    elif choiceMenu2 == 2:
+                        arrayConfAvanzada[1] = input("Ingrese el nuevo valor para el 'Tipo de cambio: valor del colon': \n> $")
+                        print(">>> Valor modificado exitosamente.")
+                    elif choiceMenu2 == 3:
+                        arrayConfAvanzada[2] = input("Ingrese el nuevo valor para el 'Tipo de cambio: valor del bitcoin': \n> $")
+                        print(">>> Valor modificado exitosamente.")
+                    elif choiceMenu2 == 4:
+                        arrayConfAvanzada[3] = input("Ingrese el nuevo valor para el 'Acumulado del tragamonedas': \n> $")
+                        print(">>> Valor modificado exitosamente.")
+                    elif choiceMenu2 == 5:
+                        arrayConfAvanzada[4] = input("Ingrese el nuevo valor para la 'Apuesta mínima del tragamonedas': \n> $")
+                        print(">>> Valor modificado exitosamente.")
+                    elif choiceMenu2 == 6:
+                        arrayConfAvanzada[5] = input("Ingrese el nuevo valor para la 'Apuesta mínima del blackjack': \n> $")
+                        print(">>> Valor modificado exitosamente.")
+                    else:
+                        print(">>> Opción no válida.")
 
-                with open(config_file, "w") as file:
-                    for value in arrayConfAvanzada:
-                        file.write(f"{value}\n")
+                    with open(config_file, "w") as file:
+                        for value in arrayConfAvanzada:
+                            file.write(f"{value}\n")
+                except Exception:
+                    print("Solo puede ingresar números. Inténtelo nuevamente.")
+
             elif choice == 3:
                 print(">>> Saliendo de la configuración avanzada...")
                 helpers.returnToMainMenu()
